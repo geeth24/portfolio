d3bea93a21fd095e5c7a2bea7fdf83c151abc12b
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-01-25 21:07:38 -0600

    Fix Redirects

--- src/components/Portfolio/Producer/Music.jsx
+++ src/components/Portfolio/Producer/Music.jsx
@@ -4,27 +4,27 @@ import Lights from "../../../images/lights.jpg";
 
 
 export const music = [
-    {
-        id: 1,
-        title: "Burning Bridges",
-        description: "Produced by me and featuring ZNO and Itihas",
-        image: BurningBridges,
-        link: "bb.itsthetechie.com",
-    },
-    {
-        id: 2,
-        title: "Boyfriend",
-        description: "Produced by me and featuring Itihas",
-        image: Boyfriend,
-        link: "boyfriend.itsthetechie.com",
-    },
-    {
-        id: 3,
-        title: "Lights",
-        description: "Produced by me and featuring MRAV",
-        image: Lights,
-        link: "lights.itsthetechie.com",
-    },
+  {
+    id: 1,
+    title: "Burning Bridges",
+    description: "Produced by me and featuring ZNO and Itihas",
+    image: BurningBridges,
+    link: "https://bb.itsthetechie.com",
+  },
+  {
+    id: 2,
+    title: "Boyfriend",
+    description: "Produced by me and featuring Itihas",
+    image: Boyfriend,
+    link: "https://boyfriend.itsthetechie.com",
+  },
+  {
+    id: 3,
+    title: "Lights",
+    description: "Produced by me and featuring MRAV",
+    image: Lights,
+    link: "https://lights.itsthetechie.com",
+  },
 ];
 
 
