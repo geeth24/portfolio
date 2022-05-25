c6ceaf4b54fc5170d1166414e1f33fb9206ec7cf
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-05-24 23:32:59 -0500

    Add New Photos

--- src/components/Portfolio/Photographer/Photographer.jsx
+++ src/components/Portfolio/Photographer/Photographer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import {
   PhotographerCardP,
@@ -8,7 +8,11 @@ import {
   PhotographerH12,
 } from "./PhotographerElements";
 
+import { ScaleFade, SlideFade, useDisclosure } from "@chakra-ui/react";
+import { useInView } from "react-intersection-observer";
+
 import PhotoAlbum from "react-photo-album";
+import "./renderDiv.css";
 
 import One from "../../../images/photographer/ReactShots 1-min.jpg";
 import Two from "../../../images/photographer/ReactShots 2-min.jpg";
@@ -26,6 +30,43 @@ import Thirteen from "../../../images/photographer/ReactShots 13-min.jpg";
 import Fourteen from "../../../images/photographer/ReactShots 14-min.jpg";
 import Fifteen from "../../../images/photographer/ReactShots 15-min.jpg";
 import Sixteen from "../../../images/photographer/ReactShots 16-min.jpg";
+import Seventeen from "../../../images/photographer/ReactShots 17-min.jpg";
+import Eighteen from "../../../images/photographer/ReactShots 18-min.jpg";
+import Nineteen from "../../../images/photographer/ReactShots 19-min.jpg";
+import Twenty from "../../../images/photographer/ReactShots 20-min.jpg";
+import TwentyOne from "../../../images/photographer/ReactShots 21-min.jpg";
+import TwentyTwo from "../../../images/photographer/ReactShots 22-min.jpg";
+import TwentyThree from "../../../images/photographer/ReactShots 23-min.jpg";
+import TwentyFour from "../../../images/photographer/ReactShots 24-min.jpg";
+import TwentyFive from "../../../images/photographer/ReactShots 25-min.jpg";
+
+import styled from "styled-components";
+
+export const renderDiv = styled.div``;
+
+const renderPhoto: RenderPhoto = (
+  {
+  layout,
+  layoutOptions,
+  imageProps: { alt, style, ...restImageProps },
+  
+  
+}) => (
+  <SlideFade initial={0.5} in={true}>
+    <renderDiv className="render-div">
+      <img
+        alt={alt}
+        style={{ ...style, width: "100%", padding: 0, transition: "all 1s" }}
+        {...restImageProps}
+      />
+      <div className="render-div-overlay">
+        <div className="render-div-overlay-text">
+          <h1>{restImageProps.Eight}</h1>
+        </div>
+      </div>
+    </renderDiv>
+  </SlideFade>
+);
 
 const images = [
   {
@@ -108,26 +149,101 @@ const images = [
     width: 175,
     height: 125,
   },
+  {
+    src: Seventeen,
+    width: 125,
+    height: 175,
+  },
+  {
+    src: Eighteen,
+    width: 175,
+    height: 125,
+  },
+  {
+    src: Nineteen,
+    width: 175,
+    height: 125,
+  },
+  {
+    src: Twenty,
+    width: 175,
+    height: 125,
+  },
+  {
+    src: TwentyOne,
+    width: 175,
+    height: 125,
+  },
+  {
+    src: TwentyTwo,
+    width: 175,
+    height: 125,
+  },
+  {
+    src: TwentyThree,
+    width: 175,
+    height: 125,
+  },
+  {
+    src: TwentyFour,
+    width: 175,
+    height: 125,
+  },
+  {
+    src: TwentyFive,
+    width: 175,
+    height: 125,
+  },
 ];
 
 const Photographer = () => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView && !isOpen) {
+      onToggle();
+    }
+  });
   return (
     <>
-      <PhotographerContainer>
+      <PhotographerContainer ref={ref}>
         <PhotographerH1>Photographer</PhotographerH1>
         <PhotographerCardP>Reactive Shots</PhotographerCardP>
-        <PhotoAlbum layout="masonry" photos={images} />
+        <SlideFade initial={0.5} in={isOpen}>
+          <PhotoAlbum
+            layout="masonry"
+            photos={images}
+            renderPhoto={renderPhoto}
+          />
+        </SlideFade>
       </PhotographerContainer>
     </>
   );
 };
 
 export const PhotographerDom = () => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView && !isOpen) {
+      onToggle();
+    }
+  });
   return (
     <>
-      <PhotographerDomContainer>
+      <PhotographerDomContainer ref={ref}>
         <PhotographerH12>Reactive Shots</PhotographerH12>
-        <PhotoAlbum layout="masonry" photos={images} />
+        <SlideFade initial={0.5} in={isOpen}>
+          <PhotoAlbum
+            layout="masonry"
+            photos={images}
+            renderPhoto={renderPhoto}
+          />
+        </SlideFade>
       </PhotographerDomContainer>
     </>
   );
